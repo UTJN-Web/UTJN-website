71a03da3a3260e2ea8447eff0edc6b37a68e35be
fuma <fuma.kano@mail.utoronto.ca>
2025-04-29 22:30:21 -0400

    Updated appearences

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,11 @@
-// app/page.tsx
+// app/about/page.tsx
 import Link from 'next/link';
 
-export default function Home() {
+export default function About() {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Welcome to Home page!</h1>
+      <h1>About Page</h1>
+      <p>This is the About page.</p>
       <nav style={{ marginTop: '20px' }}>
         <Link href="/">Home</Link> | <Link href="/about">About</Link>
       </nav>
