3a00afca9ac504531a0223f790ee0ecbda8b4f01
kentsploch <kenta.ploch@mail.utoronto.ca>
2025-05-20 18:48:53 -0400

    Deleted attribute password

--- authentication/entity/user.py
+++ authentication/entity/user.py
@@ -2,17 +2,10 @@ from dataclasses import dataclass, field
 from typing import Any, Dict
 
 
-class User:
-    username: str
-    password: str
+class Login_User:
     email: str
     token: str = field(default="")
     # The token is optional and defaults to an empty string.
     def __init__(self, username: str, password: str, email: str, token: str = ""):
-        self.username = username
-        self.password = password
         self.email = email
         self.token = token
-
-
-
