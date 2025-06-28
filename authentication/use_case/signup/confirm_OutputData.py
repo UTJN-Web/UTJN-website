38871d0985d1f766548b5a461be57d60a93c32c2
kentsploch <kenta.ploch@mail.utoronto.ca>
2025-06-28 13:57:59 -0400

    Added Input and Output Data for Signup

---
+++ authentication/use_case/signup/confirm_OutputData.py
@@ -0,0 +1,10 @@
+class ConfirmOutputData:
+    def __init__(self, success: bool, message: str):
+        self.success = success
+        self.message = message
+
+    def is_success(self) -> bool:
+        return self.success
+    
+    def get_message(self) -> str:
+        return self.message
---
+++ authentication/use_case/signup/signup_OutputData.py
@@ -0,0 +1,10 @@
+class SignupOutputData:
+    def __init__(self, success: bool, message: str):
+        self.success = success
+        self.message = message
+
+    def is_success(self) -> bool:
+        return self.success
+    
+    def get_message(self) -> str:
+        return self.message
