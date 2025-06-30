ff4a102312cd07be549684d035d2f647052e5dea
fuma <fuma.kano@mail.utoronto.ca>
2025-06-30 09:31:30 -0400

    fixed bug in database

--- lib/initPrisma.ts
+++ lib/initPrisma.ts
@@ -6,24 +6,16 @@ import {
   } from "@aws-sdk/client-secrets-manager";
   import { PrismaClient } from "@prisma/client";
   
-  const secret_name = "rdsdb-2454f7d1-b6f2-4366-95f6-1ccf8b4be221"; // Secrets Manager の名前
-  const client = new SecretsManagerClient({ region: "us-east-2" });
-  
   let prisma: PrismaClient;
   
-  export async function initPrisma() {
-    const response = await client.send(
-      new GetSecretValueCommand({ SecretId: secret_name })
-    );
-  
-    const secret = JSON.parse(response.SecretString!);
+  export async function initPrisma() {  
   
     // URLエンコードで安全な形式に
-    const encodedPassword = encodeURIComponent(secret.password);
+    const encodedPassword = encodeURIComponent(".7yS:(eUe<Zrw9hFO_ne1BHU1p1o");
   
     // DATABASE_URL を実行時に動的に設定
-    process.env.DATABASE_URL = `postgresql://${secret.username}:${encodedPassword}@${secret.host}:${secret.port}/${secret.dbname}`;
-  
+    process.env.DATABASE_URL = `postgresql://postgres:${encodedPassword}@utjn-db.ch68m8sgy7on.us-east-2.rds.amazonaws.com:5432/utjn`;
+
     prisma = new PrismaClient();
   }
   
